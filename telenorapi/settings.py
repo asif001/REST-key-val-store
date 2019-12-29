5a33155e863e7841e63287a005b45a8a14c938dc
Asifur Rahman <asifurarahman@gmail.com>
2019-12-29 23:06:40 +0600

    Changed Debug to False

--- telenorapi/settings.py
+++ telenorapi/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-b_h19oiipko@v^e$tz^y5c!(z_#a%)o9tkb4*c9*#7%f&^m61'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
